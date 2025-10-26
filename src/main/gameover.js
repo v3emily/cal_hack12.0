3c6bdbb4d87eeece3778c15976de3fac581ba888
Danny <228867636+dnnyrms@users.noreply.github.com>
2025-10-26 10:06:49 -0700

    add restart button to game over

--- src/main/gameover.js
+++ src/main/gameover.js
@@ -2,6 +2,6 @@ const startButton = document.getElementById('restartbtn');
 startButton.addEventListener('click', switchProject);
 
 function switchProject(event) {
-    window.location.href="./../cory/index.html";
+    window.location.href="./cory/index.html";
     // window.location.href="./overworld.html";
 }
