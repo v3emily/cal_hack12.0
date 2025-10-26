ec76fec67460e359dc455ccc51a92a014067b8cb
Danny <228867636+dnnyrms@users.noreply.github.com>
2025-10-26 08:12:52 -0700

    integrate cory hall mini game

---
+++ src/main/title/title.js
@@ -0,0 +1,6 @@
+const startButton = document.getElementById('startbtn');
+startButton.addEventListener('click', switchProject);
+
+function switchProject(event) {
+    window.location.href="./../cory/index.html";
+}
