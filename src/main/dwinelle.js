64048ef5791a4467433c71ddf399a01cd339438a
Danny <228867636+dnnyrms@users.noreply.github.com>
2025-10-26 04:52:01 -0700

    add background logic and in progress javascript

--- src/main/dwinelle.js
+++ src/main/dwinelle.js
@@ -1,1 +1,38 @@
+speed = 5;
 
+window.onload=function() {
+    canvas = document.getElementById('canvas1');
+    ctx = canvas.getContext('2d');
+    setInterval(update, 1000/30);
+    let player = new Player();
+    canvas.addEventListener('keypress', function(event) {
+        if (event.key == 'a') {
+            player.x += speed;
+        } else if (event.key == 'd') {
+            player.x -= speed;
+        }
+    });
+}
+
+function gameOver() {
+
+}
+
+function update() {
+    // would want to add logic for following circle here
+    ctx.fillStyle='black';
+    ctx.fillRect(0,0,canvas.width, canvas.width);
+    ctx.fillStyle='red';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+class Player {
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.width = 50;
+        this.height = 50;
+        this.image = new Image();
+        this.image.src = 'mar_1.png';
+    }
+}
